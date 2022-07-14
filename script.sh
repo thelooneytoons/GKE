d82dbb440a0108bb911a6e1bf6f132ab4cb78be0
thelooneytoons <gaura3v@gmail.com>
2022-07-14 15:46:35 +0530

    edited script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-./cloud_sql_proxy -instances=thelooneytoons-tasks:us-central1:sql-instance-8=tcp:3306 &
+# ./cloud_sql_proxy -instances=thelooneytoons-tasks:us-central1:sql-instance-8=tcp:3306 &
 python3 connect.py;
