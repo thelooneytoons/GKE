b7933d59eeb709a5e0d2dc511b8e4c558606684e
thelooneytoons <gaura3v@gmail.com>
2022-07-15 16:18:35 +0530

    oauth_scopes updated

--- gke.tf
+++ gke.tf
@@ -8,8 +8,7 @@ resource "google_container_cluster" "primary" {
     machine_type    = var.machine_type
     service_account = google_service_account.service_account_1.email
     oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/cloud-platform",
     ]
 
     metadata = {
